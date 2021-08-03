5d92ca23170f0d8097835c5d71bfced8cc71dbd2
marianaplazas <mariana.plazas@mercadolibre.com.co>
2021-08-03 01:03:19 -0500

    add returns with strings

--- fragments/functions.py
+++ fragments/functions.py
@@ -1,8 +1,8 @@
-
 def individual_maze(ser, egoismo):
-    lideal = ser[0]
-    lsubjetivo = ser[1]
-    lreal = ser[2]
+    egoismo = int(egoismo[0])
+    lideal = int(ser[0])
+    lsubjetivo = int(ser[1])
+    lreal = int(ser[2])
 
     a = abs(lsubjetivo - lreal)
     b = (lideal - lreal)
@@ -20,13 +20,14 @@ def individual_maze(ser, egoismo):
 
     enunciado = ("dado el modelo este es el valor de cada una de las categorías, Placer {}, Virtud {}, Conocimiento {}").format(a,abs(b),g)
     if (recomtotal == False):
-        return(enunciado + " " + recomvir)
-    return(enunciado + " " + recomvir + " " + individual)
+        return(enunciado + " " + recomvir, individual)
+    return(enunciado + " " + recomvir + " " + individual, indivdual)
 
 def world_maze(mundo, altruismo):
-    lideal = mundo[0]
-    lsubjetivo = mundo[1]
-    lreal = mundo[2]
+    altruismo = int(altruismo[1])
+    lideal = int(mundo[0])
+    lsubjetivo = int(mundo[1])
+    lreal = int(mundo[2])
 
 
     a = abs(lsubjetivo - lreal)
@@ -34,11 +35,13 @@ def world_maze(mundo, altruismo):
     g = abs(lreal - lsubjetivo)
 
     world = (a + b + g) * altruismo
-
+    return(world)
+   
 def loved_maze(laberintopasion, altruismo):
-    lideal = laberintopasion[0]
-    lsubjetivo = laberintopasion[1]
-    lreal = laberintopasion[2]
+    altruismo = int(altruismo[1])
+    lideal = int(laberintopasion[0])
+    lsubjetivo = int(laberintopasion[1])
+    lreal = int(laberintopasion[2])
 
 
     a = abs(lsubjetivo - lreal)
@@ -46,16 +49,19 @@ def loved_maze(laberintopasion, altruismo):
     g = abs(lreal - lsubjetivo)
 
     loved = (a + b + g) * altruismo
+    return(loved)
 
-def lambda(individual, world, loved):
+def lambd(individual, world, loved):    
     if (loved < 5):
         lamb = indvidual + world
+        return(lamb)
     else:
         lamb = individual + loved
+        return(lamb)
 
-def anguish(lamb, amor, verlaine, rimbaud):
+def anguish(lamb, amor, verlaine):
     lis = []
     omega = lamb/amor
-    for i in range(verlaine, rimbaud):
-        l.append(lamb * i)
-
+    anguish = omega * verlaine
+    strang = "Tú total de angustia es {}".format(anguish)
+    return(strang)
--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template,abort ,request
+from fragments import functions 
+
 app = Flask(__name__)
 
 @app.route('/',methods=["GET","POST"])
@@ -8,12 +10,17 @@ def inicio():
 @app.route('/procesar', methods=['POST', 'GET'])
 def procesar():
 	if request.method == 'POST':
-		ser = (request.form.get('pregunta-ser-1'),request.form.get('pregunta-ser-1'),request.form.get('pregunta-ser-1'))
-		mundo = (request.form.get('pregunta-mundo-1'),request.form.get('pregunta-mundo-1'),request.form.get('pregunta-mundo-1'))
+		ser = (request.form.get('pregunta-ser-1'),request.form.get('pregunta-ser-2'),request.form.get('pregunta-ser-3'))
+		mundo = (request.form.get('pregunta-mundo-1'),request.form.get('pregunta-mundo-2'),request.form.get('pregunta-mundo-3'))
 		altruismoegoismo = (request.form.get('pregunta-alt-1'),request.form.get('pregunta-alt-2'))
 		laberintospasion = (request.form.get('pregunta-laberintos-1'),request.form.get('pregunta-laberintos-2'),request.form.get('pregunta-laberintos-3'))
 		amor = request.form.get('pregunta-amor-1')
 		verlaine = request.form.get('pregunta-Verlaine-1')
-		return '<p>{0} {1} {2} {3} {4} {5}</p>'.format(ser, mundo, altruismoegoismo, laberintospasion, amor, verlaine)
+		world = (functions.world_maze(mundo, altruismoegoismo))
+		(string, value) = functions.individual_maze(ser, altruismoegoismo)
+		loved = functions.loved_maze(laberintospasion,altruismoegoismo)
+		lam = functions.lambd(value, world, loved)
+		anguish = functions.anguish(lam,int(amor), int(verlaine))
+		return '<p>{0}, {1},</p>'.format(string, anguish)
 
 app.run("0.0.0.0",5000,debug=True)
